163c22602c51d7237ea8415f809b5275f25b6e62
Rajath12e <38193362+Rajath12e@users.noreply.github.com>
2024-03-17 23:22:37 -0400

    Project update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+(Summation of a series) Write a program that displays the result of
+				1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+*/
+
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);;
+
     }
 }
